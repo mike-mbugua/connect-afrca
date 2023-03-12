186e167cedff533b003b5777d6710b993e6295d2
michael-mbugua <kamirimichael369@gmail.com>
2023-03-12 23:27:47 +0300

    initial code for view the page

---
+++ src/compponents/forms/ViewBooking.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './ViewBooking.css'
+import Logo from './../../images/Logo.png'
+function ViewBooking() {
+return (
+    <div className='mybooking-container'>
+        
+        <div className='login'>
+        <div className='login-info'>
+            <img src={Logo} alt="" /> 
+            <div >Connect Africa</div>
+        </div>
+            <form action="">
+                <label htmlFor="">Email</label>
+                <input type="email" />
+                <label htmlFor="">Secret Code</label>
+                <input type="password" />
+                <button className='view'>View </button>
+                <p>Have not booked yet  ?<a href="/booking">click here</a></p>
+            </form>
+        </div>
+    </div>
+)
+}
+
+export default ViewBooking
