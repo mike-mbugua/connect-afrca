7b30655e3070dd7d0bfd3577aa94fee33676ecf6
michael-mbugua <kamirimichael369@gmail.com>
2023-03-12 21:35:15 +0300

    initial code for bookinng forms

---
+++ src/compponents/forms/Book.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Book.css'
+function Book() {
+return (
+    <div className='book-forms'>
+        <div className="short-info">
+            <p>Fill the form below to be able to book the visit.
+            Your information is safe with us
+            <br />
+            </p>
+        </div>
+        <div className="twoforms">
+                <form action="" className='form1'>
+                    
+                    <label htmlFor="">First Name</label>
+                    <input type="text" />
+                    <label htmlFor="">Email</label>
+                    <input className='inputs' type="email" placeholder='example@gmail.com' />
+                    <label htmlFor="">Type Of Visit</label>
+                    <input type="text" /> 
+                    <label htmlFor="">Number Of Adults</label>
+                    <input type="text" />
+                    <label htmlFor=""> Date of Arrival</label>
+                    <input type="date" />
+                    <textarea placeholder='Tell us something You would like us to know about you' name="" id="" cols="30" rows="10"></textarea>
+        </form>
+            
+        <form action="">
+            <label htmlFor="">Last Name</label>
+            <input type="text" /> 
+            <label htmlFor="">Phone Number</label>
+            <input type="text" />
+            <label htmlFor="">Type Of Experience</label>
+            <input type="text" /> 
+            <label htmlFor="">Number Of Children </label>
+            <input type="text" />
+            <label htmlFor="">Departure Date </label>
+            <input type="date" />
+            <label htmlFor="">Secret Code</label>
+            <input type="password"  placeholder=' Do not Share it with anyone'/>
+            <button>Submit</button>
+        </form>
+        </div>
+    </div>
+)
+}
+
+export default Book
