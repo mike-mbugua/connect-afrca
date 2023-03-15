67b0720a547d48e1832d23e6406412c33942babf
michael-mbugua <kamirimichael369@gmail.com>
2023-03-15 11:21:13 +0300

    add culture components

---
+++ src/compponents/Culture/Culture.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Card from '../card/Card'
+import './Culture.css'
+function Culture() {
+return (
+    <div className='culture-main'>
+        <Card
+        img={Uganda2}
+        country="Uganda"
+        heading={'Sarova hotel '}
+        btn={" 30,000"}
+        list1={'wifi'}
+        list2={'wifi'}
+        list3={"Parking"}
+        list4={'wifi'}
+        list5={'wifi'}
+        list6={"Parking"}
+        list7={'wifi'}
+        list8={'wifi'}/>
+
+<Card
+        img={KenyaL2}
+        country="Kenya"
+        heading={'Sarova hotel '}
+        button="3000"
+        btn={" 30,000"}
+        list1={'wifi'}
+        list2={'wifi'}
+        list3={"Parking"}
+        list4={'wifi'}
+        list5={'wifi'}
+        list6={"Parking"}
+        list7={'wifi'}
+        list8={'wifi'}/>
+
+<Card
+        img={Tanzania1}
+        country="Tanzania"
+        heading={'Sarova hotel '}
+        button="3000"
+        btn={" 30,000"}
+        list1={'wifi'}
+        list2={'wifi'}
+        list3={"Parking"}
+        list4={'wifi'}
+        list5={'wifi'}
+        list6={"Parking"}
+        list7={'wifi'}
+        list8={'wifi'}/>
+
+<Card
+        img={Tanzania2}
+        country="Tanzania"
+        heading={'Sarova hotel '}
+        button="3000"
+        btn={" 30,000"}
+        list1={'wifi'}
+        list2={'wifi'}
+        list3={"Parking"}
+        list4={'wifi'}
+        list5={'wifi'}
+        list6={"Parking"}
+        list7={'wifi'}
+        list8={'wifi'}/>
+
+<Card
+        img={Rwanda1}
+        country="Rwanda"
+        heading={'Sarova hotel '}
+        button="3000"
+        btn={" 30,000"}
+        list1={'wifi'}
+        list2={'wifi'}
+        list3={"Parking"}
+        list4={'wifi'}
+        list5={'wifi'}
+        list6={"Parking"}
+        list7={'wifi'}
+        list8={'wifi'}/>
+
+    </div>
+)
+}
+
+export default Culture
